7d971eaf09e6edd62aba3ea25f63edb933d564e2
paulloy <paulloy020896@gmail.com>
2020-07-15 19:48:03 +0000

    Navbar toggle icon when clicked will bring up the navbar mobile menu. Styling applied to navbar-mobile in header.css; and navbarToggle.js updated with more code so that the navbar displays the correct css classes and id values

--- assets/js/navbarToggle.js
+++ assets/js/navbarToggle.js
@@ -1,9 +1,24 @@
-$("#navbarToggle").click(function(){ // This will add classes that animates the navbarToggle icon
-    $("#top-bar").toggleClass("top-bar");
-    $("#middle-bar").toggleClass("middle-bar");
-    $("#bottom-bar").toggleClass("bottom-bar");
-    $("#navbar").toggleClass("mobile-nav");
-    $("nav").toggle();
-    $("#navbar").toggleAttr("id")
+$("#navbarToggle").click(function () {
+  // This will add classes that animates the navbarToggle icon
+  $("#top-bar").toggleClass("top-bar");
+  $("#middle-bar").toggleClass("middle-bar");
+  $("#bottom-bar").toggleClass("bottom-bar");
+
+  if ($("#navbar").attr("id") != $("navbar")) {
+    $("#navbar").attr("id", "navbar-mobile");
+  } else {
+    $("#navbar-mobile").attr("id", "navbar");
+  }
+  $("nav").toggle();
+  $("#navbar-mobile").slideToggle();
+
+  $("#follow-us").toggle(
+    function () {
+      $("#follow-us").text("Follow Us");
+    }
+  );
 });
 
+/*if (("#navbarToggle").attr("style") == "display: none;") {        Use event listener
+    $("#navbar").attr("style", "display: block;");
+}*/
