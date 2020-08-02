687634bada989c936f1bfe3c64a1ec64ec268faf
paulloy <paulloy020896@gmail.com>
2020-08-02 12:41:15 +0000

    index.js file created and linked to on index.html. The category-list, object-list, and property-list will now toggle the children ul elements when clicked

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+$("#category-list").click(function(){
+    $("#category-list ul").slideToggle();
+});
+$("#object-list").click(function(){
+    $("#object-list ul").slideToggle();
+});
+$("#property-list").click(function(){
+    $("#property-list ul").slideToggle();
+});
