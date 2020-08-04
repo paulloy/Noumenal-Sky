6448132d75016def0f212040f84019864bc7f7c8
paulloy <paulloy020896@gmail.com>
2020-08-04 20:22:48 +0000

    Now only one of the three lists contained in #information-container will display at a time. When a new list is clicked, the others will close

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,30 @@
-$("#category-list").click(function(){
-    $("#category-list ul").slideToggle();
+$("#category-list p").click(function(){
+    $("#category-list ul").toggle();
+    $("#object-list ul").hide();
+    $("#property-list ul").hide();
 });
-$("#object-list").click(function(){
-    $("#object-list ul").slideToggle();
+$("#object-list p").click(function(){
+    $("#object-list ul").toggle();
+    $("#category-list ul").hide();
+    $("#property-list ul").hide();
 });
-$("#property-list").click(function(){
-    $("#property-list ul").slideToggle();
+$("#property-list p").click(function(){
+    $("#property-list ul").toggle();
+    $("#object-list ul").hide();
+    $("#category-list ul").hide();
 });
+
+/*var xhr = new XMLHttpRequest();
+
+xhr.onload = function() {
+    if(xhr.status === 200) {
+        responseObject = JSON.parse(xhr.responseText);
+        console.log(this.responseText);
+    }
+    else {
+        console.log("what is wrong");
+    }
+}
+
+xhr.open("GET", "/json/space-catalog.json", true);
+xhr.send(null);*/
