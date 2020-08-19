ae742546f2c69a196a7f5dda571b43f454d9e6e6
paulloy <paulloy020896@gmail.com>
2020-08-19 20:24:50 +0000

    JS shortened. Unused data and list var removed. openCloseLists function had last two lines changed to one. New var 'category' added, that allowed me to remove if else statements by replacing some keys with this var. onClick attr removed from index.html and replaced with jQuery click

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,12 @@
 function openCloseLists(listElement) {
   var listElement; //category-list, object-list, property-list
-  $("#" + listElement).find("p").click(function () {
+  $("#" + listElement)
+    .find("p")
+    .click(function () {
       //when the listElements daughter <p> element is clicked, run function
       var root = $(this).parent(); // set root as the parent of the <p> element
       root.find("ul").toggle(); // find the <ul> child element and toggle it
-      root.siblings().first().find("ul").hide(); // find the two siblings of the listElement and hide their <ul> elements
-      root.siblings().last().find("ul").hide();
+      root.siblings().find("ul").hide(); // find the two siblings of the listElement and hide their <ul> elements
     });
 }
 openCloseLists("category-list"); // run this function for each listElement clicked
@@ -19,14 +20,21 @@ $("#explanation-container").click(function () {
   $(this).hide();
 });
 
-$("#top-container").click(function () { //toggle in mobile view
+$("#top-container").click(function () {
+  //toggle in mobile view
   $("#image-information").toggle();
   $("#image-information").toggleClass("mobile-display-image-info");
 });
 
 // ----------------------------------------------------
 
-var data;
+$("#category-list ul li").click(function () { //run this function when a category list item is clicked
+  var categoryId = "#" + $(this).attr("id"); //get id of current list item and add "#" to start of it
+  $("#object-list ul").empty(); //empty any values currently displayed in object list
+  getData(categoryId); 
+  $("#object-list ul").show(); //display object list
+  $("#category-list ul").hide(); //hide category list after being clicked
+});
 
 function getListById(id) {
   var listById = document.getElementById(id).childNodes[3];
@@ -37,104 +45,57 @@ var jsonUrlStart = "assets/js/json/categories/";
 
 function getData(categoryId) {
   var xhr = new XMLHttpRequest(); //Make a new XML Http Request
+  var category = categoryId.substring(1); //remove "#" from string
 
-  if (categoryId === "#planets") {
-    //get data depending on which category is clicked
-    xhr.open("GET", jsonUrlStart + "planets.json");
-  } else if (categoryId === "#dwarf") {
-    xhr.open("GET", jsonUrlStart + "dwarf.json");
-  } else if (categoryId === "#moons") {
-    xhr.open("GET", jsonUrlStart + "moons.json");
-  } else if (categoryId === "#other") {
-    xhr.open("GET", jsonUrlStart + "other.json");
-  }
+  xhr.open("GET", jsonUrlStart + category + ".json");
   xhr.send();
 
   xhr.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      /*cb*/ var obj = JSON.parse(this.responseText);
+      var obj = JSON.parse(this.responseText);
 
-      if (categoryId === "#planets") {
-        for (var i = 0; i < obj.planets.length; i++) {
-          displayCategoryList("planets");
+      for (var i = 0; i < obj[category].length; i++) {
+        displayCategoryList(category);
 
-          $("#" + i).click(function () {
-            //when list item is selected the following click function will get id attribute and call function
-            var getId = $(this).attr("id");
-            setObjectInfo(getId, "planets");
-          });
-        }
-      } else if (categoryId === "#dwarf") {
-        for (var i = 0; i < obj.dwarf.length; i++) {
-          displayCategoryList("dwarf");
-
-          $("#" + i).click(function () {
-            var getId = $(this).attr("id");
-            setObjectInfo(getId, "dwarf");
-          });
-        }
-      } else if (categoryId === "#moons") {
-        for (var i = 0; i < obj.moons.length; i++) {
-          displayCategoryList("moons");
-
-          $("#" + i).click(function () {
-            var getId = $(this).attr("id");
-            setObjectInfo(getId, "moons");
-          });
-        }
-      } else if (categoryId === "#other") {
-        for (var i = 0; i < obj.other.length; i++) {
-          displayCategoryList("other");
-
-          $("#" + i).click(function () {
-            var getId = $(this).attr("id");
-            setObjectInfo(getId, "other");
-          });
-        }
-      }
-
-      function displayCategoryList(cat) {
-        var node = document.createElement("LI"); //create list node
-        var textnode = document.createTextNode(obj[cat][i].name); //create text node
-        node.appendChild(textnode); //append text node to list
-        node.setAttribute("id", i); //set id
-        getListById("object-list").appendChild(node); //append the list to the listById
-      }
-
-      var list = document.getElementById("object-list").childNodes[3]
-        .childNodes;
-      var el = document.getElementById("image"); //sets img element with id of image as a variable
-
-      function setObjectInfo(i, cat) {
-        //i = array index; cat = category key (planets, moons, dwarf, other)
-        el.removeAttribute("src"); // remove src attribute
-        el.setAttribute("src", obj[cat][i].image); // set src attribute to contain the image values from json
-        el.setAttribute("alt", obj[cat][i].alt); // set alt attribute to contain the alt values from json
-        $("#image-information article h2").text(obj[cat][i].name); // set text of <h2> in #image-information to value of name from json
-        $("#image-information article p").text(obj[cat][i].alt); // set text of <p> in #image-information to value of name from json
-        $("#property-values").empty(); // empty all content from #property-values
-        var createArticle = document.createElement("ARTICLE"); // create an <article> node, set as variable
-        var createH2 = document.createElement("H2"); // create a <h2> node, set as variable
-        var createParagraph = document.createElement("P"); // create a <p> node, set as variable
-        var h2TextNode = document.createTextNode(obj[cat][i].name); // create a text node containing name value from json, set as variable
-        var paragraphTextNode = document.createTextNode(obj[cat][i].about); // create a text node containing about value from json, set as variable
-        createH2.appendChild(h2TextNode); // The following 4 lines will append the text nodes to their respective <h2> or <p> node, then append it all to the <article> node
-        createParagraph.appendChild(paragraphTextNode);
-        createArticle.appendChild(createH2);
-        createArticle.appendChild(createParagraph);
-        document.getElementById("property-values").appendChild(createArticle); // Get #property-values and append the createArticle variable
-        $("#object-list ul").hide(); // Hide the list after it is clicked
+        $("#" + i).click(function () { //when list item is selected the following click function will get id attribute and call function
+          var getId = $(this).attr("id");
+          setObjectInfo(getId, category);
+        });
       }
     }
-  };
-}
 
-function onCategoryClick(categoryId) {
-  //onclick function will run when list is clicked
-  $("#object-list ul").empty();
-  getData(categoryId);
-  $("#object-list ul").show();
-  $("#category-list ul").hide();
+    function displayCategoryList(cat) {
+      var node = document.createElement("LI"); //create list node
+      var textnode = document.createTextNode(obj[cat][i].name); //create text node
+      node.appendChild(textnode); //append text node to list
+      node.setAttribute("id", i); //set id
+      getListById("object-list").appendChild(node); //append the list to the listById
+    }
+
+    var el = document.getElementById("image"); //sets img element with id of image as a variable
+
+    function setObjectInfo(i, cat) {
+      //i = array index; cat = category key (planets, moons, dwarf, other)
+      el.removeAttribute("src");
+      el.removeAttribute("alt");
+      el.setAttribute("src", obj[cat][i].image); // set src attribute to contain the image values from json
+      el.setAttribute("alt", obj[cat][i].alt); // set alt attribute to contain the alt values from json
+      $("#image-information article h2").text(obj[cat][i].name); // set text of <h2> in #image-information to value of name from json
+      $("#image-information article p").text(obj[cat][i].alt); // set text of <p> in #image-information to value of name from json
+      $("#property-values").empty(); // empty all content from #property-values
+      var createArticle = document.createElement("ARTICLE"); // create an <article> node, set as variable
+      var createH2 = document.createElement("H2"); // create a <h2> node, set as variable
+      var createParagraph = document.createElement("P"); // create a <p> node, set as variable
+      var h2TextNode = document.createTextNode(obj[cat][i].name); // create a text node containing name value from json, set as variable
+      var paragraphTextNode = document.createTextNode(obj[cat][i].about); // create a text node containing about value from json, set as variable
+      createH2.appendChild(h2TextNode); // The following 4 lines will append the text nodes to their respective <h2> or <p> node, then append it all to the <article> node
+      createParagraph.appendChild(paragraphTextNode);
+      createArticle.appendChild(createH2);
+      createArticle.appendChild(createParagraph);
+      document.getElementById("property-values").appendChild(createArticle); // Get #property-values and append the createArticle variable
+      $("#object-list ul").hide(); // Hide the list after it is clicked
+    }
+  };
 }
 
 // Organise this code
