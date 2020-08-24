e321caa00c5a087595de082d9ed99acdb67365c4
paulloy <paulloy020896@gmail.com>
2020-08-24 20:12:24 +0000

    The nav element when open on widths less than 750px will be prepended to the body. When closed it will be appended to the header. Animation added so now when opened the nav will slide onto the screen from the left while the rest of the body is pushed over the right edge of the screen. The reverse animation will run when the navbar icon is clicked again

--- assets/js/navbarToggle.js
+++ assets/js/navbarToggle.js
@@ -1,19 +1,34 @@
 // Functional but needs fine tuning so it can be really good
 
-function navbarToggle() {
-  //When called this function will toggle between two css classes for the navbar
-  $("#navbar").toggleClass("navbar-browser"); //Default display of navbar-browser is block;
-  $("#navbar").toggleClass("navbar-mobile"); //Default display of navbar-mobile is none;
-}
 function navbarIsOpen() {
+  $("#navbar-cover").fadeIn();
+  $("#navbar").toggleClass("navbar-mobile"); //Default display of navbar-mobile is none;
+  $("#navbar").toggleClass("navbar-browser"); //Default display of navbar-browser is block;
+
+  $("body").prepend($("nav"));
+  $("html").children().animate({ left: "250px" }, 500);
+
   $("#top-bar").removeClass("top-bar-close");
   $("#middle-bar").removeClass("middle-bar-close");
   $("#bottom-bar").removeClass("bottom-bar-close");
   $("#top-bar").addClass("top-bar-open"); //This class will rotate the top bar
   $("#middle-bar").addClass("middle-bar-open"); //This class will display the middle bar as none
   $("#bottom-bar").addClass("bottom-bar-open"); //This class will rotate the bottom bar
+
+  $("#navbar").animate({ left: "0px" }, 500);
 }
 function navbarIsClosed() {
+  $("#navbar-cover").fadeOut();
+  setTimeout(function () {
+    $("#navbar").toggleClass("navbar-browser"); //Default display of navbar-browser is block;
+    $("#navbar").toggleClass("navbar-mobile"); //Default display of navbar-mobile is none;
+  }, 510);
+
+  $("html").children().animate({ left: "0" }, 500);
+  $("#navbar").animate({ left: "-250px" }, 500, function () {
+    $("header").append($("nav"));
+  });
+
   $("#top-bar").removeClass("top-bar-open");
   $("#middle-bar").removeClass("middle-bar-open");
   $("#bottom-bar").removeClass("bottom-bar-open");
@@ -25,8 +40,12 @@ function toggleButtonAnimation(clickCount) {
   oddEven = clickCount % 2;
   if (oddEven === 1) {
     navbarIsOpen();
+    $("nav").show();
   } else if (oddEven === 0) {
     navbarIsClosed();
+    setTimeout(function () {
+      $("nav").hide();
+    }, 510);
   }
   //When called this function will toggle the classes that animates the navbar toggle icon
 }
@@ -34,38 +53,39 @@ function toggleButtonAnimation(clickCount) {
 // Figure out a method better than nested if else statements
 
 document.getElementsByTagName("body")[0].onresize = function () {
-  navbarIsClosed();
+  $("html").children().css("left", "0px");
+  $("header").append($("nav"));
+  $("#navbar-cover").fadeOut();
+  $("#top-bar").removeClass("top-bar-open");
+  $("#middle-bar").removeClass("middle-bar-open");
+  $("#bottom-bar").removeClass("bottom-bar-open");
+  $("#top-bar").addClass("top-bar-close"); //This class will rotate the top bar
+  $("#middle-bar").addClass("middle-bar-close"); //This class will display the middle bar as none
+  $("#bottom-bar").addClass("bottom-bar-close"); //This class will rotate the bottom bar
   clickCount = 0;
   //This event listener will run when the screen size changes. Such as when a user rotates their mobile or tablet
   var windowWidth = window.innerWidth; //Gets value of window width in px
   if (windowWidth > 750) {
-    //If window width is larger than 750px, the nested if statement will run
-
-    if (
-      document.getElementById("navbar").hasAttribute("class", "navbar-mobile")
-    ) {
+    //If window width is larger than 750px, the nested if statement will run     
       $("#navbar").attr("class", "navbar-browser");
-    } //The switch from navbar-mobile to navbar-browser is incase a user switches from a screen larger than 750px to less than.
+      $("#navbar").removeAttr("style"); //stops displaying wrong css
+     //The switch from navbar-mobile to navbar-browser is incase a user switches from a screen larger than 750px to less than.
 
     //$(".navbar-browser").css("display", "flex"); (probably not needed now)
   } else if (windowWidth <= 750) {
-    if (
-      document.getElementById("navbar").hasAttribute("class", "navbar-mobile")
-    ) {
+    
       $("#navbar").attr("class", "navbar-browser"); //change class
       $("nav").css("display", "none"); //display nav to none
       $("#top-bar").removeClass("top-bar");
       $("#middle-bar").removeClass("middle-bar"); //remove classes so a cross doesn't appear when the navbar is closed.
       $("#bottom-bar").removeClass("bottom-bar");
-      $("$#navbar").removeAttr("style"); //stops displaying wrong css
+      $("#navbar").removeAttr("style"); //stops displaying wrong css
     }
-  }
+  
 };
 
 var clickCount = 0;
-$("#navbarToggle").click(function () {
+$("#navbarToggle, #navbar-cover").click(function () {
   clickCount++;
   toggleButtonAnimation(clickCount);
-  navbarToggle();
-  $("nav").toggle();
 });
