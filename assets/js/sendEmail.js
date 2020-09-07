c5d1b2011e0d2f153f3da09899625a6b8abd5c16
paulloy <paulloy020896@gmail.com>
2020-09-07 17:52:52 +0000

    JS on about.html has been added to sendEmail.js; When the form is submitted the input, textarea, and button elements will be disabled so a user can't send the same message multiple times unless they reload the page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,3 +1,21 @@
+(function () {
+  emailjs.init("user_Bhu3aze5EPVtC6BlZNdyc");
+})();
+
+$("#form").submit(function () {
+  return sendEmail(this);
+});
+$("#form").submit(function () {
+  $("#name").attr("disabled", "disabled");
+  $("#name").val("");
+  $("#email").attr("disabled", "disabled");
+  $("#email").val("");
+  $("#message").attr("disabled", "disabled");
+  $("#message").val("");
+  $("#submit").attr("disabled", "disabled");
+  $("#submit").val("");
+});
+
 function sendEmail(contactForm) {
   emailjs
     .send("gmail", "noumenal_sky_contact", {
