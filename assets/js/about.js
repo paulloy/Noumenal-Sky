8a4eb5f89d62b072c434b69d6a007e2805c286f7
paulloy <paulloy020896@gmail.com>
2020-09-10 19:20:07 +0000

    The page has been redesigned. The Q&A will now display as an accordian on the left half of the window. On the right half of the window will be displayed the contact form. When the window width is less than 900 px, the contact form will hide and the Q&A will take up the whole width. A new button appears that when clicked will hide the Q&A and display the contact form, where there will be a button that will bring back the Q&A and hide the form

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,1 +1,25 @@
-console.log('hellow world');
+$(".title").click(function () {
+  $(this).next().slideToggle();
+});
+
+$("#contact").click(function() {
+    $(this).hide();
+    $("#q-and-a").hide();
+    $("#qA").show();
+    $("#form").show();
+});
+$("#qA").click(function() {
+    $(this).hide();
+    $("#form").hide();
+    $("#q-and-a").show();
+    $("#contact").show();
+});
+
+document.getElementsByTagName("body")[0].onresize = function () {
+    if (window.innerWidth > 900) {
+        $("#q-and-a").removeAttr("style");
+        $("#form").removeAttr("style");
+        $("#qA").removeAttr("style");
+        $("#contact").removeAttr("style");
+    }
+}
