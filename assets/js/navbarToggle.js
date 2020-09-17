402b01571264f0bdfcc3d55e263827b6cdc1ffe6
paulloy <paulloy020896@gmail.com>
2020-09-17 21:24:58 +0000

    Navbar animation changed to 200ms

--- assets/js/navbarToggle.js
+++ assets/js/navbarToggle.js
@@ -2,12 +2,12 @@
 
 function navbarIsOpen() { 
   $("html, body").css("position", "fixed"); //Display the body as fixed.
-  $("#navbar-cover").fadeIn(); // Fade in the navbar-cover
+  $("#navbar-cover").fadeIn(200); // Fade in the navbar-cover
   $("#navbar").toggleClass("navbar-mobile"); //Default display of navbar-mobile is none;
   $("#navbar").toggleClass("navbar-browser"); //Default display of navbar-browser is block;
 // Toggle between the two navbar css styles.
   $("body").prepend($("nav")); //prepend the nav to the body when it is being opened.
-  $("html").children().animate({ left: "250px" }, 500); //Animate the body so that it slides to the right of the screen.
+  $("html").children().animate({ left: "250px" }, 200); //Animate the body so that it slides to the right of the screen.
 
 
   $("#top-bar").removeClass("top-bar-close"); //The following classes are responsible for the navbar toggle icon animation
@@ -17,21 +17,21 @@ function navbarIsOpen() {
   $("#middle-bar").addClass("middle-bar-open"); //This class will display the middle bar as none
   $("#bottom-bar").addClass("bottom-bar-open"); //This class will rotate the bottom bar
 
-  $("#navbar").animate({ left: "0px" }, 500); // animate the navbar so it slides to the right, giving the look of pushing the body off the screen.
+  $("#navbar").animate({ left: "0px" }, 200); // animate the navbar so it slides to the right, giving the look of pushing the body off the screen.
 }
 function navbarIsClosed() {
-  $("#navbar-cover").fadeOut(); //hide the navbar cover when the navbar is being closed.
+  $("#navbar-cover").fadeOut(200); //hide the navbar cover when the navbar is being closed.
 
-  $("html, body").children().animate({ left: "0" }, 500); // move the body back to the left.
-  $("#navbar").animate({ left: "-250px" }, 500); // move the navbar to the left.
+  $("html, body").children().animate({ left: "0" }, 200); // move the body back to the left.
+  $("#navbar").animate({ left: "-250px" }, 200); // move the navbar to the left.
   setTimeout(function () {
     $("header").append($("nav")); //append the nav back to the header.
-  }, 500);
+  }, 200);
 
   setTimeout(function () {
     $("#navbar").toggleClass("navbar-browser"); //Default display of navbar-browser is block;
     $("#navbar").toggleClass("navbar-mobile"); //Default display of navbar-mobile is none;
-  }, 500); // Gives time for the navbar to close before toggling the classes.
+  }, 200); // Gives time for the navbar to close before toggling the classes.
 
  
 
@@ -43,7 +43,7 @@ function navbarIsClosed() {
   $("#bottom-bar").addClass("bottom-bar-close"); //This class will rotate the bottom bar
   setTimeout(function () {
     $("html, body").css("position", "static");
-  }, 500);
+  }, 200);
 }
 function toggleButtonAnimation(clickCount) {
     /* When the navbar is opened the clickCount will be odd, so oddEven will be 1. When closed, clickCount will be even
@@ -55,8 +55,8 @@ function toggleButtonAnimation(clickCount) {
   } else if (oddEven === 0) {
     navbarIsClosed();
     setTimeout(function () { // When navbar is being closed, run the navbarIsClosed function on line 24.
-      $("nav").hide(); // After 500ms hide the nav. This gives the nav time to run its closing animation before hiding.
-    }, 500);
+      $("nav").hide(); // After 200ms hide the nav. This gives the nav time to run its closing animation before hiding.
+    }, 200);
   }
   //When called this function will toggle the classes that animates the navbar toggle icon
 }
@@ -66,7 +66,7 @@ function toggleButtonAnimation(clickCount) {
 document.getElementsByTagName("body")[0].onresize = function () {
   $("html, body").children().css("left", "0px");
   $("header").append($("nav"));
-  $("#navbar-cover").fadeOut();
+  $("#navbar-cover").fadeOut(200);
   $("#top-bar").removeClass("top-bar-open");
   $("#middle-bar").removeClass("middle-bar-open");
   $("#bottom-bar").removeClass("bottom-bar-open");
