49b2b16fc26b8c983c5ce55f209975b259b40af2
paulloy <paulloy020896@gmail.com>
2020-10-08 18:06:17 +0000

    Fixed: bug that caused error on form submit has been fixed

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,18 +1,19 @@
 /*globals $:false */
 /*jshint esversion: 6 */
+var emailjs;
 
 //This allows a user to send an email using Email.js
-let emailjs;
 (function () {
   emailjs.init("user_Bhu3aze5EPVtC6BlZNdyc");
 })();
 
 $("#form").submit(function () {
-  //After submission, disable and clear the form so that multiple emails are not sent and a user knows the message was successfully sent.
-  $("#name, #email, #message, #submit").attr("disabled", "disabled").val("");
   return sendEmail(this); //Send email on form submit.
 });
-$("#form").submit(function () {});
+$("#form").submit(function () {
+  //After submission, disable and clear the form so that multiple emails are not sent and a user knows the message was successfully sent.
+  $("#name, #email, #message, #submit").attr("disabled", "disabled").val("");
+});
 
 function sendEmail(contactForm) {
   emailjs.send("gmail", "noumenal_sky_contact", {
